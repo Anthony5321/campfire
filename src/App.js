1b46a42df675f99ef72f647f708b85fec140cd2f
anthony5321 <tyharpo12@gmail.com>
2023-03-28 17:03:43 -0400

    init pages and creating the routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import Client from './services/api';
+import LandingPage from './pages/landingPage/LandingPage';
+import Home from './pages/home/Home';
 
 function App() {
+
+  const [user, setUser] = useState(null)
+
+  const [story, setStory] = useState([])
+  const getStories = async () => {
+    try {
+      const res = await Client.get('/stories')
+      setStory(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const CheckSession = async () => {
+    try {
+      const res = await Client.get('/user/session')
+      return res.data
+    } catch (error) {
+      throw error
+    }
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    getMeals()
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes className="Routes">
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/home' element={<Home user={user} setUser={setUser} />} />
+        <Route path="/about" element={<About user={user} setUser={setUser} />} />
+        <Route path="/stories/:id" element={
+          <StoryDetails story={story} getStories={getStories} user={user} setUser={setUser} />
+        } />
+        <Route path="/add/story" element={<AddStory user={user} getStories={getStories} setUser={setUser} />} />
+        <Route path='/auth' element={<UserDash user={user} setUser={setUser} />}></Route>
+        <Route path='/edit' element={<EditStory user={user} setUser={setUser} />}></Route> 
+      </Routes>
     </div>
   );
 }
