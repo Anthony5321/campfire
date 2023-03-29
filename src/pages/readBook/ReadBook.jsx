0586308886b1360171801857bb783556c368c129
anthony5321 <tyharpo12@gmail.com>
2023-03-29 13:21:54 -0400

    Got my snippets to call each other on the front

--- src/pages/readBook/ReadBook.jsx
+++ src/pages/readBook/ReadBook.jsx
@@ -1,55 +1,62 @@
-import { useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import './ReadBook.css'
-import Nav from '../../components/Nav'
-import { Link } from 'react-router-dom'
-import Client from '../../services/api'
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import './ReadBook.css';
+import Nav from '../../components/Nav';
+import { Link } from 'react-router-dom';
+import Client from '../../services/api';
 
 const ReadBook = () => {
-    let { id } = useParams();
-    const [snippet, setSnippet] = useState({});
-    const [allSnippets, setAllSnippets] = useState([]);
-  
-    const GetSnippet = async (nextSnippetIds) => {
-        try {
-          const res = await Client.get(`/snippets/snippet/${nextSnippetIds[0]}`);
-          setSnippet(res.data);
-          console.log(res.data[0].image);
-          setAllSnippets(res.data.children);
-        } catch (err) {
-          console.log(err);
-        }
-      };
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        await GetSnippet(['1']);
-      };
-      fetchData();
-    }, []);
-  
-    return (
-      <div>
-        <Nav />
-        <img src={snippet[0].image} alt="image" />
-        <p>{snippet[0].content}</p>
-        {allSnippets && allSnippets.length > 0 && (
-  <>
-    <button onClick={() => GetSnippet(allSnippets[0].nextSnippetIds)}>
-      {allSnippets[0].content}
-    </button>
-    <button onClick={() => GetSnippet(allSnippets[1].nextSnippetIds)}>
-      {allSnippets[1].content}
-    </button>
-  </>
-)}
-        <div className="backBtn">
-          <Link to={"/home"} className="backLink">
-            <p>Back</p>
-          </Link>
-        </div>
-      </div>
-    );
+  let { id } = useParams();
+  const [snippet, setSnippet] = useState({});
+  const [allSnippets, setAllSnippets] = useState([]);
+
+  const GetSnippet = async (snippetId) => {
+    try {
+      const res = await Client.get(`/snippets/snippet/${snippetId}`);
+      if (res.data && res.data.length > 0) {
+        setSnippet(res.data[0]);
+        console.log(res.data[0]);
+        setAllSnippets(res.data[0].children);
+      } else {
+        console.log('No data found');
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
-  
-  export default ReadBook;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await GetSnippet(id);
+    };
+    fetchData();
+  }, [id]);
+
+  return (
+    <div>
+      <Nav />
+      {snippet && (
+        <>
+          <img src={snippet.image} alt="image" />
+          <p>{snippet.content}</p>
+          {allSnippets && allSnippets.length > 0 && (
+            <>
+              {allSnippets.map((child) => (
+                <button key={child.id} onClick={() => GetSnippet(child.id)}>
+                  {child.content}
+                </button>
+              ))}
+            </>
+          )}
+        </>
+      )}
+      <div className="backBtn">
+        <Link to={'/home'} className="backLink">
+          <p>Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ReadBook;
