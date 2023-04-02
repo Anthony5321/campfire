64aae1ef044c2ffe01179e88e1457442ca1eae62
anthony5321 <tyharpo12@gmail.com>
2023-04-02 16:07:52 -0400

    Another minor fix to make the app deploy

--- src/pages/readBook/ReadBook.jsx
+++ src/pages/readBook/ReadBook.jsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom';
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import './ReadBook.css';
 import { Link } from 'react-router-dom';
 import Client from '../../services/api';
